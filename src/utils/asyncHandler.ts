c8436e27c94ee893a545ad7468a851d7348c0e65
arkajitroy <arkajitroy11.official@gmail.com>
2026-08-06 12:28:46 +0530

    removed legacy files and folders, recommence project

--- src/utils/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { NextFunction, Request, RequestHandler, Response } from 'express';
-
-export const asyncHandler =
-  (handler: (req: Request, res: Response, next: NextFunction) => Promise<unknown>): RequestHandler =>
-  (req, res, next) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
-  };
