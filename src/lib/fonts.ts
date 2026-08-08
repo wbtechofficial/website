b9f61fe39f824ad256e3926a95dbfc2b6556b94e
arkajitroy <arkajitroy11.official@gmail.com>
2026-08-08 12:45:29 +0530

    feat: addup + modification of the new font to the website

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,12 +1,23 @@
-import { Montserrat, Geist_Mono } from "next/font/google";
+import { Montserrat, Geist_Mono, Poppins, Inter } from "next/font/google";
 
 export const montserrat = Montserrat({
-    variable: "--font-montserrat",
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
+export const popins = Poppins({
+  variable: "--font-popins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 export const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
