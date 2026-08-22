75ff17219b8d151d6a5f628e21df35096007d474
arkajitroy <arkajitroy11.official@gmail.com>
2026-08-22 12:07:04 +0530

    fix: build issue pnpm-lockfile

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,5 @@
-import { CommunitiesSection } from "@/@module/home/components/communities-section";
-import { CompaniesSection } from "@/@module/home/components/companies-section";
-
 import { DevelopersSection } from "@/@module/home/components/developers-section";
-import { EventsSection } from "@/@module/home/components/events-section";
 import { HeroSection } from "@/@module/home/components/hero-section";
-import { StartupsSection } from "@/@module/home/components/startups-section";
 
 export default async function Home() {
     return (
